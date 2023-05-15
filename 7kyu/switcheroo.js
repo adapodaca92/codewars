cd5892a68863402db7fea22fce3d00a8f681c3c8
Anthony Apodaca <adapodaca92@gmail.com>
2023-05-15 00:07:31 -0700

    Completed Switcheroo 7kyu.

---
+++ 7kyu/switcheroo.js
@@ -0,0 +1,23 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x) {
+  let stringToArray = x.split('');
+  stringToArray.forEach((letter, i) => {
+    if (letter === 'a') {
+      stringToArray[i] = 'b';
+    } else if (letter === 'b') {
+      stringToArray[i] = 'a';
+    } else {
+      stringToArray[i] = 'c';
+    }
+  });
+  return stringToArray.join('');
+}
+
+// console.log(switcheroo('abc'));
+switcheroo('abbac');
