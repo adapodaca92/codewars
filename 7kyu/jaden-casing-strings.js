45704d15cec29f74e2355f78fab28dda5f76958a
Anthony Apodaca <adapodaca92@gmail.com>
2023-06-25 01:33:04 -0700

    Refactored Jaden Casing String 7kyu.

--- 7kyu/jaden-casing-strings.js
+++ 7kyu/jaden-casing-strings.js
@@ -1,14 +1,8 @@
 String.prototype.toJadenCase = function () {
   //...
-  let quoteArray = this.split(' ');
-  console.log(quoteArray);
-  for (let i = 0; i < quoteArray.length; i++) {
-    quoteArray[i] =
-      quoteArray[i].charAt(0).toUpperCase() + quoteArray[i].slice(1);
-  }
-  console.log(quoteArray);
-  quoteArray = quoteArray.join(' ');
-  return quoteArray;
+  return this.split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 };
 
 let newQuote = "How can mirrors be real if our eyes aren't real";
